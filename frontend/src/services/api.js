8c086db71bc6299825059deca9d30740cbe4a4d0
Stwinki <stwinkidoo@gmail.com>
2026-04-08 20:18:36 -0600

    Conectando frontend con backend en la nube

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://catalogo-inversiones-rubi.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
